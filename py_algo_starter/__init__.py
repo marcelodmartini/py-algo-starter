a633ba1f8d99140696230a5bacd1349ea4a7781b
Naranjax <naranjax@MacBookPro.fibertel.com.ar>
2025-11-03 21:17:49 -0300

    Actualizo fetch automático y config YAML

--- py_algo_starter/__init__.py
+++
@@ -1,3 +0,0 @@
-from .run_backtest import run_once
-
-__all__ = ["run_once"]
--- py_algo_starter/env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-WEB_SERVICE_BASE_URL = os.getenv("WEB_SERVICE_BASE_URL", "").rstrip("/")
-UPLOAD_TOKEN = os.getenv("UPLOAD_TOKEN", "")
-REPORTS_DIR = os.getenv("REPORTS_DIR", "/tmp/reports")
-TZ = os.getenv("TZ", "UTC")
